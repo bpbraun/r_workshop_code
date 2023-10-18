d1e43cb1dfa259321b084db0f7f6791bee47e7df
Benjy Braun <benjaminpbraun@gmail.com>
2023-10-18 13:54:19 -0400

    Simplify correlationa analysis and plotting

--- main.r
+++ main.r
@@ -12,48 +12,33 @@ avg_solar <- aggregate(Solar.R ~ Month, data, mean)
 avg_solar
 
 # Analysis Part 2: Correlation between Ozone and Solar Radiation for each month
-correlation_May <- cor(data$Ozone[data$Month == 5], data$Solar.R[data$Month == 5])
-print(paste("Correlation for May: ", correlation_May))
+correlations <- sapply(unique(data$Month), function(m) {
+  cor(data$Ozone[data$Month == m], data$Solar.R[data$Month == m])
+})
 
-correlation_June <- cor(data$Ozone[data$Month == 6], data$Solar.R[data$Month == 6])
-print(paste("Correlation for June: ", correlation_June))
-
-correlation_July <- cor(data$Ozone[data$Month == 7], data$Solar.R[data$Month == 7])
-print(paste("Correlation for July: ", correlation_July))
-
-correlation_August <- cor(data$Ozone[data$Month == 8], data$Solar.R[data$Month == 8])
-print(paste("Correlation for August: ", correlation_August))
-
-correlation_September <- cor(data$Ozone[data$Month == 9], data$Solar.R[data$Month == 9])
-print(paste("Correlation for September: ", correlation_September))
+names(correlations) <- c("May", "June", "July", "August", "September")
+print(correlations)
 
 # Visualization
-plot_shapes <- vector("character", length=nrow(data))
-for (i in 1:nrow(data)) {
-    if (correlation_May > 0.5) {
-        plot_shapes[i] <- 19
-    } else if (correlation_June > 0.5) {
-        plot_shapes[i] <- 17
-    } else {
-        plot_shapes[i] <- 15
-    }
+shape_assignment <- function(cor_value) {
+  if (cor_value > 0.5) {
+    return(19)
+  } else if (cor_value > 0 & cor_value <= 0.5) {
+    return(17)
+  } else {
+    return(15)
+  }
 }
 
 # Save Plots
-g1 <- ggplot(data[data$Month == 5,], aes(x = Solar.R, y = Ozone)) + geom_point(aes(shape = factor(Month))) + ggtitle("May")
-ggsave("plot_may.png", g1)
-
-g2 <- ggplot(data[data$Month == 6,], aes(x = Solar.R, y = Ozone)) + geom_point(aes(shape = factor(Month))) + ggtitle("June")
-ggsave("plot_june.png", g2)
-
-g3 <- ggplot(data[data$Month == 7,], aes(x = Solar.R, y = Ozone)) + geom_point(aes(shape = factor(Month))) + ggtitle("July")
-ggsave("plot_july.png", g3)
-
-g4 <- ggplot(data[data$Month == 8,], aes(x = Solar.R, y = Ozone)) + geom_point(aes(shape = factor(Month))) + ggtitle("August")
-ggsave("plot_august.png", g4)
-
-g5 <- ggplot(data[data$Month == 9,], aes(x = Solar.R, y = Ozone)) + geom_point(aes(shape = factor(Month))) + ggtitle("September")
-ggsave("plot_september.png", g5)
+for (m in unique(data$Month)) {
+  month_name <- month.abb[m]
+  shape <- shape_assignment(correlations[month_name])
+  g <- ggplot(data[data$Month == m,], aes(x = Solar.R, y = Ozone)) + 
+    geom_point(shape = shape) + 
+    ggtitle(month_name)
+  ggsave(paste0("plot_", tolower(month_name), ".png"), g)
+}
 
 # Save data
 write.csv(data, "cleaned_data.csv", row.names = FALSE)
